88e33edb2b94a0c874bac3dd847f70dc1e71092c
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-06-26 09:21:55 +0530

    26 June - Learned some concepts

---
+++ 02. Graphs/01. Find if Path Exists in Graph.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/find-if-path-exists-in-graph/
+
+public boolean DFS(ArrayList<Integer> adj[], boolean[] vis, int src, int dest){
+        if(src == dest) return true;
+        
+        vis[src] = true;
+        for(int nbr : adj[src]){
+            if(vis[nbr] == false){
+                if(DFS(adj, vis, nbr, dest)) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        
+        // STEP-1 create a 'visited array'
+        boolean[] vis = new boolean[n];
+        
+        // STEP-2 create an 'adjacency list'
+        ArrayList<Integer>[] adj = new ArrayList[n];
+        for(int i = 0; i < adj.length; i++)
+            adj[i] = new ArrayList<>();
+        for(int[] e : edges){
+            adj[e[0]].add(e[1]); // agar '0' se '1' ek edge hai ...
+            adj[e[1]].add(e[0]); // toh '1' se '0' bhi ek edge hai
+        }
+        
+        // STEP-3 apply DFS on all of the neighbouring vertices
+        return DFS(adj, vis, source, destination);
+    }
+}
