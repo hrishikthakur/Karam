588284fe2cab95ba7e3229072a9fe7da641e1494
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-07-01 09:24:35 +0530

    j = i + 1
    https://practice.geeksforgeeks.org/problems/transpose-of-matrix-1587115621/1

---
+++ 04. Arrays/04. Transpose of Matrix.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    static void transpose(int matrix[][], int n)
+    {
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
