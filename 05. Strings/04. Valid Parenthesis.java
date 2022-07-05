e35b4eec814bc8cab888d970bf4f4b0394565dd0
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-07-05 12:29:57 +0530

    check stk.size() != 0 prior to stk.peek() / pop()
    https://leetcode.com/problems/valid-parentheses/

---
+++ 05. Strings/04. Valid Parenthesis.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stk = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') stk.push(ch);
+            else if(ch == ')'){
+                if(stk.size() == 0 || stk.peek() != '(') return false;
+                else stk.pop();
+            }else if(ch == '}'){
+                if(stk.size() == 0 || stk.peek() != '{') return false;
+                else stk.pop();
+            }else if(ch == ']'){
+                if(stk.size() == 0 || stk.peek() != '[') return false;
+                else stk.pop();
+            }
+        }
+        
+        return (stk.size() == 0) ? true : false;
+    }
+}
