43aabfed492abb0a95be6a13545fbdd59477cfc5
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-07-29 17:19:09 +0530

    DFS -> Depth/Height

--- 01. Trees/12. Check Height Balanced (IB).java
+++ 01. Trees/12. Check Height Balanced (IB).java
@@ -1,3 +1,23 @@
+// ****************************** O(N^2) ***************************************
+
+// 1) Each nd every node has to chechked for "isBalanced"
+class Solution {
+    
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+    
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        if(Math.abs(lh - rh) <= 1 && isBalanced(root.left) == true && isBalanced(root.right) == true) return true;
+        return false;
+    }
+}
+
+// ****************************** O(N) ***************************************
 class Solution {
     
     static class Pair{
