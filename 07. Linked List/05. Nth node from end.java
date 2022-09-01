46ee62527996a629031300aa4618aa1fbf92741b
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-01 06:32:48 +0530

    while(fast != null && n-- > 0)
    https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1

--- 07. Linked List/05. Nth node from end.java
+++ 07. Linked List/05. Nth node from end.java
@@ -29,31 +29,22 @@ class GfG
 // Hare and Tortoise
 class GfG
 {
-    int getLength(Node head){
-        if(head == null) return 0;
-        int len = 0;
-        Node temp = head;
-        while(temp != null){
-            temp = temp.next;
-            len++;
-        }
-        return len;
-    }
-    
+   
     int getNthFromLast(Node head, int n)
     {
-        int len = getLength(head);
-        if(len < n) return -1;
-    	Node slow = head;
-    	Node fast = head;
-    	for(int i = 0; i < n; i++)
-    	    fast = fast.next;
-    	    
-    	 while(fast != null){
-    	     slow = slow.next;
-    	     fast = fast.next;
-    	 }
-    	 
-    	 return slow.data;
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast != null && n-- > 0)
+            fast = fast.next;
+
+        if(n > 0) return -1;
+        
+        while(fast != null){
+             slow = slow.next;
+             fast = fast.next;
+        }
+
+        return slow.data;
     }
 }
