d9eeb153b2b4d8e4697c9d19d2eec5112b70ec25
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-17 22:44:13 +0530

    Easy Peasy
    https://leetcode.com/problems/two-sum/

--- 08. Two Pointer/01. Two Sum.java
+++ 08. Two Pointer/01. Two Sum.java
@@ -1,4 +1,4 @@
-// Brute -> Time:O(n^2), Space:O(1)
+// Brute -> Using 2 pointer -> Time:O(n^2), Space:O(1)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
@@ -16,9 +16,29 @@ class Solution {
     }
 }
 
-// 2 Traversal using HashMaps -> Time:O(2 * n), Space:O(n)
+// 2 Traversal -> Using HashMap -> Time:O(2 * n), Space:O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        
+        int[] res = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            int remTarget = target - nums[i];
+            if(map.containsKey(remTarget) == true && map.get(remTarget) != i){
+                res[0] = i;
+                res[1] = map.get(remTarget);
+                return res;
+            }
+        }
+        
+        return res;
+    }
+}
 
-// Single Traversal using HashMap -> Time:O(n), Space:O(n)
+// Single Traversal -> Using HashMap -> Time:O(n), Space:O(n)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
