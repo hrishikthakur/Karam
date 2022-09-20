f4598939a44e3e0b90ee63b4791eccdd41ff6358
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-20 14:52:26 +0530

    Easy Peasy
    No link to practice

---
+++ 08. Two Pointer/09. Count ALL Pairs > K.java
@@ -0,0 +1,16 @@
+public int twoSum(int[] nums, int k){
+  Arrays.sort(nums);
+  int pairCount = 0;
+  int lo = 0, hi = nums.length - 1;
+  while(lo < hi){
+    int sum = nums[lo] + nums[hi];
+    if(sum > k){
+      pairCount = hi - lo;
+      hi--;
+    }
+    else{
+      lo++;
+    }
+  }
+  return pairCount;
+}
