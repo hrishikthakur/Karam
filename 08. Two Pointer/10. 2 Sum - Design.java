e525a402b1f3fdedf206c97d33a948eb9c8b2eff
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-20 16:09:30 +0530

    Easy Peasy
    https://www.lintcode.com/problem/607/

---
+++ 08. Two Pointer/10. 2 Sum - Design.java
@@ -0,0 +1,35 @@
+public class TwoSum {
+    ArrayList<Integer> data;
+    boolean isSorted;
+
+    public TwoSum(){
+        data = new ArrayList<>();
+        isSorted = true;
+    }
+
+    public void add(int number) {
+        data.add(number);
+        isSorted = false;
+    }
+
+    public boolean find(int k) {
+        if(isSorted == false){
+            Collections.sort(data);
+            isSorted = true;
+        }
+
+        int lo = 0, hi = data.size() - 1;
+        while(lo < hi){
+            int sum = data.get(lo) + data.get(hi);
+            if(sum == k){
+                return true;
+            }else if(sum < k){
+                lo++;
+            }else{
+                hi--;
+            }
+        }
+
+        return false;
+    }
+}
