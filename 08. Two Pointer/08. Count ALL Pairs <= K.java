b6d824eb6fed8a9560b549799b5bb13eda83dfe6
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-20 14:52:40 +0530

    Easy Peasy
    No link to practice

--- 08. Two Pointer/08. Count ALL Pairs <= K.java
+++ 08. Two Pointer/08. Count ALL Pairs <= K.java
@@ -1,11 +1,11 @@
 public int twoSum(int[] nums, int k){
   Arrays.sort(nums);
-  int 
+  int pairCount = 0;
   int lo = 0, hi = nums.length - 1;
   while(lo < hi){
     int sum = nums[lo] + nums[hi];
     if(sum <= k){
-      pairCount = hi - lo;
+      pairCount += hi - lo;
       lo++;
     }
     else{
