025a7824dd5076233b66ad66d36a61a7ea650fa6
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-25 15:01:39 +0530

    K Sum
    https://leetcode.com/problems/4sum/

--- 08. Two Pointer/20. 4Sum.java
+++ 08. Two Pointer/20. 4Sum.java
@@ -1,5 +1,5 @@
+// Loop + Loop + 2 Pointer -> Time:O(N^3), Space:O(1)
 class Solution {
-    
     public static List<List<Integer>> twoSumUnique(int[] nums, int lo, int hi, long target){
         List<List<Integer>> pairs = new ArrayList<>();
         int start = lo;
@@ -46,5 +46,59 @@ class Solution {
         }
         
         return res;
+    }
+}
+
+// Using kSum -> Time:O(N ^ k - 1), Space:O(1)
+class Solution {
+    
+    public List<List<Integer>> twoSum(int[] nums, int lo, int hi, long target){
+        List<List<Integer>> pairs = new ArrayList<>();
+        int start = lo;
+        while(lo < hi){
+            if(lo > start && nums[lo - 1] == nums[lo]){
+                lo++;
+                continue;
+            }
+            long sum = nums[lo] + nums[hi];
+            if(sum == target){
+                List<Integer> pair = new ArrayList<>();
+                pair.add(nums[lo]);
+                pair.add(nums[hi]);
+                pairs.add(pair);
+                lo++;
+                hi--;
+            }else if(sum < target){
+                lo++;
+            }else{
+                hi--;
+            }
+        }
+        return pairs;
+    }
+    
+    public List<List<Integer>> kSum(int[] nums, int start, int k, long target){
+        if(k == 2){
+            return twoSum(nums, start, nums.length - 1, target);
+        }
+        
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = start; i < nums.length; i++){
+            if(i > start && nums[i - 1] == nums[i]){
+                continue;
+            }
+            
+            List<List<Integer>> pairs = kSum(nums, i + 1, k - 1, target - nums[i]);
+            for(List pair : pairs){
+                pair.add(0, nums[i]);
+                res.add(pair);
+            }
+        }
+        return res;
+    }
+    
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, 0, 4, target);
     }
 }
