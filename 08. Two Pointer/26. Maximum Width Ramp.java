fdee6ee11bdffba6cf6c7d4cd03a5bb780932aee
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-27 17:19:14 +0530

    Suffix Array
    https://leetcode.com/problems/maximum-width-ramp/

---
+++ 08. Two Pointer/26. Maximum Width Ramp.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        int[] r_max = new int[n];
+        r_max[n - 1] = nums[n - 1];
+        for(int i = n - 2; i >= 0; i--){
+            r_max[i] = Math.max(nums[i], r_max[i + 1]);
+        }
+        
+        int ans = 0;
+        int i = 0, j = 0;
+        while(i < n && j < n){
+            if(nums[i] <= r_max[j]){
+                ans = Math.max(ans, j - i);
+                j++;
+            }else i++;
+        }
+        
+        return ans;
+    }
+}x
