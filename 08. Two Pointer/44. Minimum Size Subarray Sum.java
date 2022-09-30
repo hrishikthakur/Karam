92daf2f5adf9f0256f12f22dd39a9fbc54865c79
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-09-30 23:39:44 +0530

    Easy Peasy
    https://leetcode.com/problems/minimum-size-subarray-sum/

--- 08. Two Pointer/44. Minimum Size Subarray Sum.java
+++ 08. Two Pointer/44. Minimum Size Subarray Sum.java
@@ -1,10 +1,11 @@
+// Time:O(2N), Space:O(1)
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int min_len = Integer.MAX_VALUE, curr_sum = 0, left = 0;
         for(int right = 0; right < nums.length; right++){
             curr_sum += nums[right];
             while(curr_sum >= target){
-                min_len = Math.min(min_len, right - left + 1);
+                min_len = Math.min(min_len, right - left + 1);2
                 curr_sum -= nums[left];
                 left++;
             }
