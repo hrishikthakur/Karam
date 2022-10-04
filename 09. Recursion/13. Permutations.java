eff977478af0e6e7cdfb67d2ce8f75c1e3fab883
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-10-04 18:54:57 +0530

    Pending Approach II
    https://leetcode.com/problems/permutations/

--- 09. Recursion/13. Permutations.java
+++ 09. Recursion/13. Permutations.java
@@ -27,3 +27,31 @@ class Solution {
         return permutations;
     }
 }
+
+// Without using permutation<Integer>, visited[]
+class Solution {
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void permute(List<List<Integer>> permutations, int[] nums, int currBox){
+        if(currBox == nums.length){
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0; i < nums.length; i++)
+                temp.add(nums[i]);
+            permutations.add(temp);
+            return;
+        }
+        for(int i = currBox; i < nums.length; i++){
+            swap(nums, currBox, i);
+            permute(permutations, nums, currBox + 1);
+            swap(nums, currBox, i);
+        }   
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> permutations = new ArrayList<>();
+        permute(permutations, nums, 0);
+        return permutations;
+    }
+}
