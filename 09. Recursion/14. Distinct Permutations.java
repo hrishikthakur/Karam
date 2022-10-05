b5fbde6f28702e0470795a2c4b784f753c55e125
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-10-05 22:13:11 +0530

    Easy Peasy
    https://leetcode.com/problems/permutations-ii/

--- 09. Recursion/14. Distinct Permutations.java
+++ 09. Recursion/14. Distinct Permutations.java
@@ -1,3 +1,5 @@
+// Boxes on level
+// Items as options
 class Solution {
     public void permuteUnique(List<List<Integer>> permutations, List<Integer> permutation, 
                               HashMap<Integer, Integer> freq, int currBox, int totalBoxes){
@@ -27,6 +29,40 @@ class Solution {
         List<List<Integer>> permutations = new ArrayList<>();
         List<Integer> permutation = new ArrayList<>();
         permuteUnique(permutations, permutation, freq, 0, nums.length);
+        return permutations;
+    }
+}
+
+// Items on level
+// Boxes as Options
+class Solution {
+    public void permuteUnique(List<List<Integer>> permutations, List<Integer> permutation,
+                              int[] nums, HashMap<Integer, Integer> lastIdx, int currItem){
+        if(currItem == nums.length){
+            List<Integer> temp = new ArrayList<>(permutation);
+            permutations.add(temp);
+            return;
+        }
+        
+        int start = lastIdx.getOrDefault(nums[currItem], -1);
+        for(int currBox = start + 1; currBox < permutation.size(); currBox++){
+            if(permutation.get(currBox) == null){
+                permutation.set(currBox, nums[currItem]);
+                lastIdx.put(nums[currItem], currBox);
+                permuteUnique(permutations, permutation, nums, lastIdx, currItem + 1);
+                lastIdx.put(nums[currItem], start);
+                permutation.set(currBox, null);
+            }
+        }
+    }
+    
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        HashMap<Integer, Integer> lastIdx = new HashMap<>();
+        List<List<Integer>> permutations = new ArrayList<>();
+        List<Integer> permutation = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++)
+            permutation.add(null);
+        permuteUnique(permutations, permutation, nums, lastIdx, 0);
         return permutations;
     }
 }
