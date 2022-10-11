1ea2edfd52b14c5b6d63b554b29f287fe3158419
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-10-11 12:29:05 +0530

    Easy but ...
    https://leetcode.com/problems/combination-sum-ii/

--- 09. Recursion/21. Coin Change - Combination - I.java
+++ 09. Recursion/21. Coin Change - Combination - I.java
@@ -1,4 +1,50 @@
 // Items on level
+class Solution {
+    public static void combine(int currCoin, List<List<Integer>> res, List<Integer> combination, 
+                               List<Integer>uniqueCoins, HashMap<Integer, Integer> freq, int target){
+        // +ve base case
+        if(target == 0){
+            List<Integer> temp = new ArrayList<>(combination);
+            res.add(temp);
+            return;
+        }
+        // -ve base case
+        if(currCoin == uniqueCoins.size()) return;
+        
+        int coin = uniqueCoins.get(currCoin);
+        // how many times the coin can be used
+        for(int i = 0; i <= freq.get(coin); i++){
+            if(target - i * coin >= 0){
+                // use the coin 
+                for(int j = 1; j <= i; j++)
+                    combination.add(coin);
+                
+                combine(currCoin + 1, res, combination, uniqueCoins, freq, target - i * coin);
+                
+                
+                for(int j = 1; j <= i; j++)
+                    combination.remove(combination.size() - 1);
+            }
+        }
+    }
+    
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<Integer> uniqueCoins = new ArrayList<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for(int i = 0; i < candidates.length; i++){
+            int coin = candidates[i];
+            if(freq.containsKey(coin) == false){
+                uniqueCoins.add(coin);
+                freq.put(coin, 1);
+            }else{
+                freq.put(coin, freq.get(coin) + 1);
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        combine(0, res, new ArrayList<>(), uniqueCoins, freq, target);
+        return res;
+    }
+}
 
 
 // Box on level -> fail
