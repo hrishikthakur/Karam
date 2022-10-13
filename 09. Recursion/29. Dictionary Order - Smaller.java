537d90b71b5b18e16b6323a62e83149f58de1fe9
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-10-14 01:20:49 +0530

    New Concept(s)
    https://hack.codingblocks.com/app/practice/1/1063/problem

---
+++ 09. Recursion/29. Dictionary Order - Smaller.java
@@ -0,0 +1,30 @@
+public class Main {
+	
+	public static void permute(boolean[] vis, String permutation, String sorted, String input){
+		if(permutation.length() == input.length()){
+			if(permutation.compareTo(input) < 0){
+				System.out.println(permutation);
+			}
+			return;
+		}
+
+		for(int i = 0; i < sorted.length(); i++){
+			if(vis[i] == false){
+				vis[i] = true;
+				permute(vis, permutation + sorted.charAt(i), sorted, input);
+				vis[i] = false;
+			}
+		}
+	}
+
+    public static void main(String args[]) {
+        Scanner scn  = new Scanner(System.in);
+		String input = scn.nextLine();
+		char[] arr = input.toCharArray();
+		Arrays.sort(arr);
+		String sorted = new String(arr);
+
+		boolean[] vis = new boolean[input.length()];
+		permute(vis, "", sorted, input);
+    }
+}
