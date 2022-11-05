e459aa4e558626ad45111547ba8a14bfbed94eb6
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-11-05 22:53:01 +0530

    Good concepts within
    https://leetcode.com/problems/path-sum-iii/description/

--- 01. Trees/15. Path Sum - III.java
+++ 01. Trees/15. Path Sum - III.java
@@ -1,3 +1,4 @@
+// O(n ^ 2) -> Each node can provide a valid targetSum
 class Solution {
     public int helper(TreeNode root, long targetSum){
         if(root == null) return 0;
@@ -10,5 +11,29 @@ class Solution {
     public int pathSum(TreeNode root, int targetSum) {
         if(root == null) return 0;
         return helper(root, targetSum) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+    }
+}
+
+// O(n) -> Using the prefix array technique
+class Solution {
+    HashMap<Long, Integer> freq;
+    public int helper(TreeNode root, int targetSum, long prefix){
+        if(root == null) return 0;
+
+        prefix += root.val;
+        int count = (freq.containsKey(prefix - targetSum)) ? freq.get(prefix - targetSum) : 0;
+        freq.put(prefix, freq.getOrDefault(prefix, 0) + 1);
+
+        count += helper(root.left, targetSum, prefix);
+        count += helper(root.right, targetSum, prefix);
+
+        freq.put(prefix, freq.getOrDefault(prefix, 0) - 1); // backtrack
+        return count;
+    }
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root == null) return 0;
+        freq = new HashMap<>();
+        freq.put(0l, 1);
+        return helper(root, targetSum, 0l);
     }
 }
