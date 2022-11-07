6e42c798fd37836b865b24dc08e469c6d6ec22c7
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2022-11-07 16:21:44 +0530

    Easy
    https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/description/

---
+++ 01. Trees/23. Find Target Node in Clone of BT.java
@@ -0,0 +1,13 @@
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original == null || cloned == null) return null;
+        if(original == target) return cloned;
+      
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        if(left != null) return left;
+        TreeNode right = getTargetCopy(original.right, cloned.right, target);
+        if(right != null) return right;
+      
+        return null;
+    }
+}
