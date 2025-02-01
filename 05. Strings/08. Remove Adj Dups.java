30367869baa9fb32098d48da5d9f6d24208fb121
Hrishik Thakur <91936864+hrishikthakur@users.noreply.github.com>
2025-02-01 17:55:39 +0530

    Update 08. Remove Adj Dups.java

--- 05. Strings/08. Remove Adj Dups.java
+++ 05. Strings/08. Remove Adj Dups.java
@@ -1,26 +1,4 @@
-// Inplace using two pointers
-
-class Solution {
-    public String removeDuplicates(String s) {
-        
-        // i -> the resultant array iterator
-        // j -> to iterate over the string
-        char[] res = s.toCharArray();
-        int i = 0;
-        for(int j = 0; j < s.length(); i++, j++){
-            res[i] = res[j];
-            if(i > 0 && res[i] == res[i - 1]){
-                i -= 2;
-            }
-        }
-        
-        return new String(res, 0, i); 
-    }
-}
-// String can be created from a char array by  "new String(char_arr, si, ei - 1)
-
-
-// O(N) auxiliary space using stack approach with StringBuilder
+// O(N)
 class Solution {
     
     public String removeDuplicates(String s) {
@@ -39,4 +17,22 @@ class Solution {
         return res.toString();
     }
 }
-// sb.toString() -> To convert 
+
+// O(N) -> Using two pointers
+class Solution {
+    public String removeDuplicates(String s) {
+        
+        // i -> the resultant array iterator
+        // j -> to iterate over the string
+        char[] res = s.toCharArray();
+        int i = 0;
+        for(int j = 0; j < s.length(); i++, j++){
+            res[i] = res[j];
+            if(i > 0 && res[i] == res[i - 1]){
+                i -= 2;
+            }
+        }
+        
+        return new String(res, 0, i); 
+    }
+}
